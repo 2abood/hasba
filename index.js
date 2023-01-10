9334a027269342ba30d7bcebf783f54483b67c46
2abood <122353585+2abood@users.noreply.github.com>
2023-01-10 12:15:35 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+function dreb(num1,num2){
+return num1*num2;
+
+}
+
+function sum(num1,num2){
+    return num1/num2;
+        
+ }
+
+ function tareh(num1,num2){
+        return num1-num2;
+               
+ }
+
+  function qesna(num1,num2){
+         return num1+num2;
+                       
+ }
+
+ function calculater(num1,num2){
+         return abuelatta(num1/num2);             
+ }
+console.log(tareh(20,13));
+console.log(calculater(16,11,sum));
